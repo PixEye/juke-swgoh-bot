0df07df361ada007db10a1a0a07ae6f4dbb9b021
PixEye <pix@pixeye.net>
2021-04-04 13:52:45 +0200

    add "gach" command

--- src/jk-bot.js
+++ src/jk-bot.js
@@ -156,8 +156,8 @@ client.on("message", (message) => {
 			richMsg = new RichEmbed().setTitle("Liste des commandes")
 				.setDescription([
 					"**Commandes utilisateur :**",
-					" abbr(eviations), aide, allycode (ac), auteur, charInfo (ci), checkMods (cm), checkUnitsGp"+
-					" (cugp), countGuildTopUnits (cgtu), dis, glCheck (glc), getUnregisteredPlayers (gup),"+
+					" abbr(eviations), aide, allycode (ac), auteur, charInfo (ci), checkMods (cm), checkUnitsGp (cugp)"+
+					", countGuildTopUnits (cgtu), dis, gacH(istory), glCheck (glc), getUnregisteredPlayers (gup),"+
 					" guildBoard (gb), guildStats (gs), help, invite, (last)evols (le), listGuildMembers (lgm),"+
 					" playerStats (ps), profile (gg), register (reg), relics, repete, self(y), shipInfo (si), start,"+
 					" stats, status, whoami, whois",
@@ -762,47 +762,61 @@ client.on("message", (message) => {
 			}
 			break;
 
+		case "gach":
+		case "gachistory":
 		case "gg":
 		case "profile":
-			if (allycode) {
-				searchStr = "p.allycode="+allycode;
-			}
-			sql = "SELECT p.* FROM `users` p WHERE "+searchStr;
-			db_pool.query(sql, (exc, result) => {
-				let msg = '';
+			{
+				let link = "";
 
-				if (exc) {
-					console.log("SQL:", sql);
-					console.log(logPrefix()+"AC Exception:", exc.sqlMessage? exc.sqlMessage: exc);
-				} else {
-					console.log(logPrefix()+result.length+" record(s) match(es):", search);
-					// console.dir(result);
-					if (result.length <= 0) {
-						msg = "No match found!";
-						console.log(logPrefix()+msg);
-						message.reply(msg);
-						if (allycode) {
-							msg = "https://swgoh.gg/p/"+allycode+"/";
+				if (allycode) {
+					searchStr = "p.allycode="+allycode;
+				}
+				sql = "SELECT p.* FROM `users` p WHERE "+searchStr;
+				db_pool.query(sql, (exc, result) => {
+					let msg = '';
+
+					if (exc) {
+						console.log("SQL:", sql);
+						console.log(logPrefix()+"AC Exception:", exc.sqlMessage? exc.sqlMessage: exc);
+					} else {
+						console.log(logPrefix()+result.length+" record(s) match(es):", search);
+						// console.dir(result);
+						if (result.length <= 0) {
+							msg = "No match found!";
+							console.log(logPrefix()+msg);
+							message.reply(msg);
+
+							if (allycode) {
+								link = command.substr(1, 1)==='a'?
+									"https://swgoh.gg/p/"+allycode+"/gac-history/":
+									"https://swgoh.gg/p/"+allycode+"/";
+								console.log(logPrefix()+link);
+								message.channel.send(link);
+							}
+						} else if (result.length > 1) {
+							lines.push("There are "+result.length+" matches:");
+							result.forEach(user => {
+								msg = user.allycode+" is allycode of: "+user.game_name;
+								console.log(logPrefix()+msg);
+								lines.push("`"+user.allycode+"` is allycode of: "+user.game_name);
+							});
+							message.reply(lines);
+						} else { // 1 result here
+							let label = command.substr(1, 1)==='a'? "GAC history": "profile";
+
+							user = result[0];
+							allycode = user.allycode;
+							link = command.substr(1, 1)==='a'?
+								"https://swgoh.gg/p/"+allycode+"/gac-history/":
+								"https://swgoh.gg/p/"+allycode+"/";
+							msg = user.game_name+"'s "+label+" is: "+link;
 							console.log(logPrefix()+msg);
 							message.channel.send(msg);
 						}
-					} else if (result.length > 1) {
-						lines.push("There are "+result.length+" matches:");
-						result.forEach(user => {
-							msg = user.allycode+" is allycode of: "+user.game_name;
-							console.log(logPrefix()+msg);
-							lines.push("`"+user.allycode+"` is allycode of: "+user.game_name);
-						});
-						message.reply(lines);
-					} else { // 1 result here
-						user = result[0];
-						msg = user.game_name+"'s profile is: ";
-						msg+= "https://swgoh.gg/p/"+user.allycode+"/";
-						console.log(logPrefix()+msg);
-						message.channel.send(msg);
 					}
-				}
-			});
+				});
+			}
 			break;
 
 		case "ggs":
@@ -840,9 +854,9 @@ client.on("message", (message) => {
 				.setDescription([
 					"**User commands:**",
 					" abbr(eviations), about, aide, allycode (ac), charInfo (ci), checkMods (cm), checkUnitsGp"+
-					" (cugp), countGuildTopUnits (cgtu), glCheck (glc), getUnregisteredPlayers (gup), guildBoard (gb),"+
-					" guildStats (gs), help, invite, (last)evols (le), listGuildMembers (lgm), playerStat (ps),"+
-					" profile (gg), register (reg), relics, repeat, say, self(y), shipInfo (si),"+
+					" (cugp), countGuildTopUnits (cgtu), gacH(istory), glCheck (glc), getUnregisteredPlayers (gup),"+
+					" guildBoard (gb), guildStats (gs), help, invite, (last)evols (le), listGuildMembers (lgm),"+
+					" playerStat (ps), profile (gg), register (reg), relics, repeat, say, self(y), shipInfo (si),"+
 					" start, stats, status, whoami, whois",
 					"**Behaviour commands:**",
 					"*Order : behave|behaviour (subcommand) (points) (user)*",
