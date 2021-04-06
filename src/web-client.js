7cb84fd9aadcb56a54e15fcf000a15e047876ca4
PixEye <pix@pixeye.net>
2021-04-07 00:11:32 +0200

    add a custom web client

---
+++ src/web-client.js
@@ -0,0 +1,176 @@
+/**
+ * @since 2021-04-04
+ * @author pix@pixeye.net
+ */
+
+// Modules and other constants:
+const HTML_PARSER = require("node-html-parser");
+const LOCUTUS = require("./locutus");
+const SEP_LEN = 99;
+const TOOLS = require("./tools");
+const WEB = require("https");
+
+// Source pages:
+// const WARSTATS_TOP_GUILDS = "https://goh.warstats.net/?sort=tw&direction=asc";
+// const PROXIMA_GUILDS = "https://goh.warstats.net/15/ProXima";
+const SWGOH_TOP_GUILDS = "https://swgoh.gg/g/";
+
+exports.parseSwgohGgGuildList = function(html, result, chunkCnt) {
+	var fileSize = html.length;
+
+	if (result.statusCode !== 200) {
+		console.warn("wget failed!");
+		return;
+	}
+
+	console.log("%sB read in %d chunk(s)", TOOLS.eng_format(fileSize), chunkCnt);
+
+	const isValid = HTML_PARSER.valid(html);
+
+	console.log("HTML page is valid?", isValid? "true": "false");
+	if (!isValid) return;
+
+	const doc = HTML_PARSER.parse(html);
+	const title =
+		doc.querySelector("html>head>title").text.trim();
+
+	const dataBlocks =
+		doc.querySelectorAll("div.content-container-primary>ul>li.p-0>a>*");
+	const Headers = ["Picture", "Name", "Descr.", "Stats"];
+	const nbHeaders = Headers.length;
+	const Stats = ["Raid points", "Members", "Profiles", "Avg arena rank", "GP"];
+
+	const nbDataBlocks = dataBlocks.length;
+
+	console.log("Page title:", title);
+	console.log("Nb data blocks:", nbDataBlocks);
+	console.log("Nb cells found:", dataBlocks.length);
+
+	var colName = "";
+	var colNum = 0;
+	var i = 0;
+
+	i = 0;
+	console.log("=".repeat(SEP_LEN)); // ===== ===== ===== ======
+	dataBlocks.forEach(node => {
+		var colName = Headers[i % nbHeaders];
+		var txt = node.text.trim().replace(/\s+/g, " ");
+
+		if (!colNum++) { ++i; return; }
+
+		if (colName === "Name") {
+			txt = node.querySelector("h3").text.trim().replace(/\s+/g, " ");
+		} else
+		if (colName === "Stats") {
+			let stats = node.querySelectorAll("strong");
+
+			txt = [];
+			stats.forEach((stat, j) => {
+				let v = (Stats[j] === "GP")? Math.round(stat.text / 1e6)+"M": stat.text;
+				txt.push(Stats[j] + ": " + v);
+			});
+			txt = "(" + stats.length + ") " + txt.join("; ");
+		}
+
+		console.log("Cell %d/col %d - %s: %s", ++i, colNum, colName, txt);
+
+		if (i % nbHeaders === 0 && i>0) {
+			console.log("-".repeat(SEP_LEN)); // ----- ----- -----
+			colNum = 0;
+		}
+	});
+};
+
+exports.parseWarstatGuildList = function(html, result, chunkCnt) {
+	var fileSize = html.length;
+
+	if (result.statusCode !== 200) {
+		console.warn("wget failed!");
+		return;
+	}
+
+	console.log("%sB read in %d chunk(s)", TOOLS.eng_format(fileSize), chunkCnt);
+
+	const isValid = HTML_PARSER.valid(html);
+
+	console.log("HTML page is valid?", isValid? "true": "false");
+	if (!isValid) return;
+
+	const doc = HTML_PARSER.parse(html);
+	const title =
+		doc.querySelector("html>head>title").text.trim();
+	const mainTableCols =
+		doc.querySelectorAll("table.highlight>thead th");
+	const nbDataBlocks =
+		doc.querySelectorAll("table.highlight>tbody tr").length;
+	const mainTableCells =
+		doc.querySelectorAll("table.highlight>tbody td");
+	const nbCols = mainTableCols.length;
+
+	console.log("Page title:", title);
+	console.log("Nb colomns found:", nbCols);
+	console.log("Nb data blocks:", nbDataBlocks);
+	console.log("Nb cells found:", mainTableCells.length);
+
+	var colTitles = [];
+	var i = 0;
+	console.log("=".repeat(SEP_LEN)); // ===== ===== ===== ======
+
+	mainTableCols.forEach(node => {
+		var txt = node.text.trim().replace(/\s+/g, " ");
+
+		colTitles.push(txt);
+		console.log("Column %d contains: %s", ++i, txt);
+	});
+	console.log("=".repeat(SEP_LEN)); // ===== ===== ===== ======
+
+	var colNum = 0;
+	i = 0;
+	mainTableCells.forEach(node => {
+		var colName = colTitles[colNum];
+		var txt = node.text.trim().replace(/\s+/g, " ");
+
+		if (!colNum++) { ++i; return; }
+
+		console.log("Cell %d/col %d (%s) contains: %s", ++i, colNum, colName, txt);
+		if (colName==="Guild") {
+			let txt2 = LOCUTUS.utf8_decode(txt);
+
+			if (txt2!==txt) {
+				console.log("Cell %d/col %d (%s) contained UTF-8: %s", i, colNum, colName, txt2);
+			}
+		}
+
+		if (i % nbCols === 0 && i>0) {
+			console.log("-".repeat(SEP_LEN)); // ----- ----- -----
+			colNum = 0;
+		}
+	});
+};
+
+exports.wget = function(url, callback) {
+	WEB.get(url, function(res) {
+		var webContent = "";
+		var chunkCnt = 0;
+
+		console.log("Got response code %d from %s", res.statusCode, url);
+
+		res.on("data", function(chunk) {
+			++chunkCnt;
+			webContent += chunk;
+		});
+
+		res.on("end", function() {
+			if (typeof callback === "function") {
+				callback(webContent, res, chunkCnt);
+			}
+		});
+	}).on("error", function(e) {
+		console.log("Got error: " + e.message);
+	});
+};
+
+// exports.wget(PROXIMA_GUILDS, exports.parseWarstatGuildList);
+exports.wget(SWGOH_TOP_GUILDS, exports.parseSwgohGgGuildList);
+
+// vim: noexpandtab
