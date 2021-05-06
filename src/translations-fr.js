b824281f9624f7db2e7efaae8b7ab50037ad65ee
PixEye <pix@pixeye.net>
2021-05-07 00:10:21 +0200

    prepare French translation (to do)

---
+++ src/translations-fr.js
@@ -0,0 +1,19 @@
+/**
+ * tools.js is the functions module for Juke's SWGoH Discord bot
+ * @author PixEye@pixeye.net
+ * @since 2021-05-05
+ */
+
+// jshint esversion: 8
+
+exports.lang = 'fr';
+
+exports.translations = {
+	"Last updated at:": "Mis à jour à&nbsp;:",
+	"Loading...": "Chargement...",
+	"Loading stopped": "Chargement arrêté",
+	"Subscribe to this app": "S'abonner à cette appli",
+	"Your name": "Votre nom"
+};
+
+// vim: noexpandtab
