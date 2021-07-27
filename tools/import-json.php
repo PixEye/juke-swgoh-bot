c6e02c1299b99b9b76b707b8206c60b4ef1e3c06
PixEye <pix@pixeye.net>
2021-07-27 22:57:04 +0200

    print all errors to STDERR

--- tools/import-json.php
+++ tools/import-json.php
@@ -19,23 +19,23 @@ $db_config = $config['db'];
 // printf("DB config: %s%s", var_export($db_config, true), PHP_EOL);
 
 if (!isSet($argv) || !is_array($argv)) {
-    fprintf(STDERR, "This is a CLI program!%s", PHP_EOL);
-    exit(1);
+	fprintf(STDERR, "This is a CLI program!%s", PHP_EOL);
+	exit(1);
 }
 
 $cmd = array_shift($argv);
 $argc = count($argv);
 // printf("argc = %d%s", $argc, PHP_EOL);
 if ($argc!==1 || $argv[0]==='-h' || $argv[0]==='--help') {
-    fprintf(STDERR, "Usage: %s <input_json_file_path>%s", basename($cmd), PHP_EOL);
-    exit(2);
+	fprintf(STDERR, "Usage: %s <input_json_file_path>%s", basename($cmd), PHP_EOL);
+	exit(2);
 }
 
 $input_file = array_shift($argv);
 $json = file_get_contents($input_file, true);
 if ($json===false) {
-    fprintf(STDERR, "Cannot read file: %s", $input_file, PHP_EOL);
-    exit(3);
+	fprintf(STDERR, "Cannot read file: %s", $input_file, PHP_EOL);
+	exit(3);
 }
 
 $export = json_decode($json, true);
@@ -45,66 +45,64 @@ printf("%d block(s) found in export:%s", $nb_blocks, PHP_EOL);
 
 $sql = '';
 forEach($export as $i => $block) {
-    printf("%2d/ Block type: %s%s", $i+1, $block['type'], PHP_EOL);
-    if ($block['type']==='table' && isSet($block['data'])) {
-        $nb_records = count($block['data']);
-        $table = $block['name'];
-        printf("%d records(s) found in data for table: '%s'.%s", $nb_records, $table, PHP_EOL);
-        if ($nb_records<=0) {
-            fprintf(STDERR, "No records to insert in DB!%s", PHP_EOL);
-            exit(4);
-        }
+	printf("%2d/ Block type: %s%s", $i+1, $block['type'], PHP_EOL);
+	if ($block['type']==='table' && isSet($block['data'])) {
+		$nb_records = count($block['data']);
+		$table = $block['name'];
+		printf("%d records(s) found in data for table: '%s'.%s", $nb_records, $table, PHP_EOL);
+		if ($nb_records<=0) {
+			fprintf(STDERR, "No records to insert in DB!%s", PHP_EOL);
+			exit(4);
+		}
 
-        $keys = array_keys($block['data'][0]);
-        array_shift($keys); // drop ID (useless in my case)
-        forEach($keys as $j => $key) {
-            $keys[$j] = '`'.$key.'`';
-        }
-        $keys = implode(', ', $keys);
-        $sql.= "INSERT INTO `$table`\n(".$keys.") VALUES";
-        // printf("First SQL lines:\n %s%s", $sql, PHP_EOL);
+		$keys = array_keys($block['data'][0]);
+		array_shift($keys); // drop ID (useless in my case)
+		forEach($keys as $j => $key) {
+			$keys[$j] = '`'.$key.'`';
+		}
+		$keys = implode(', ', $keys);
+		$sql.= "INSERT INTO `$table`\n(".$keys.") VALUES";
+		// printf("First SQL lines:\n %s%s", $sql, PHP_EOL);
 
-        $values = array();
-        forEach($block['data'] as $j => $record) {
-            // printf("%2d/ record: %s%s", $j+1, var_export($record, true), PHP_EOL);
+		$values = array();
+		forEach($block['data'] as $j => $record) {
+			// printf("%2d/ record: %s%s", $j+1, var_export($record, true), PHP_EOL);
 
-            $vals = array_values($record);
-            array_shift($vals); // drop ID (useless in my case)
+			$vals = array_values($record);
+			array_shift($vals); // drop ID (useless in my case)
 
-            forEach($vals as $k => $val) {
-                if (!is_numeric($val)) $vals[$k] = '"'.$val.'"';
-            }
-            $vals = implode(', ', $vals);
-            $values[] = "($vals)";
-        }
-        printf('%s', PHP_EOL);
-        // printf("Last SQL line:\n (%s)%s", $vals, PHP_EOL);
-        printf("First and last SQL lines:\n\n%s\n...\n(%s)%s%s", $sql, $vals, PHP_EOL, PHP_EOL);
-        $sql.= "\n".implode(",\n", $values);
-        printf("SQL now contains %d line feeds.%s", substr_count($sql, "\n"), PHP_EOL);
-    }
+			forEach($vals as $k => $val) {
+				if (!is_numeric($val)) $vals[$k] = '"'.$val.'"';
+			}
+			$vals = implode(', ', $vals);
+			$values[] = "($vals)";
+		}
+		printf('%s', PHP_EOL);
+		// printf("Last SQL line:\n (%s)%s", $vals, PHP_EOL);
+		printf("First and last SQL lines:\n\n%s\n...\n(%s)%s%s", $sql, $vals, PHP_EOL, PHP_EOL);
+		$sql.= "\n".implode(",\n", $values);
+		printf("SQL now contains %d line feeds.%s", substr_count($sql, "\n"), PHP_EOL);
+	}
 }
 
 $mysqli = new mysqli($db_config['host'], $db_config['user'], $db_config['pw'], $db_config['name']);
 
-// Check connection:
-if ($mysqli->connect_errno) {
-    printf("Connection failed: %s%s", $mysqli->connect_error, PHP_EOL);
-    exit(5);
+if ($mysqli->connect_errno) { // Check connection
+	fprintf(STDERR, "Connection failed: %s%s", $mysqli->connect_error, PHP_EOL);
+	exit(5);
 }
 
 // Execute SQL query:
 if ($result = $mysqli->query($sql)) {
-    printf("Result is about %d line(s).%s", $result->affected_rows, PHP_EOL);
+	printf("Result is about %d line(s).%s", $result->affected_rows, PHP_EOL);
 
-    // Free result memory:
-    $result->close();
+	$result->close(); // Free result memory
 } else {
-    fprintf(STDERR, "DB error: %s%s", $mysqli->error, PHP_EOL);
+	fprintf(STDERR, "DB error: %s%s", $mysqli->error, PHP_EOL);
 }
 
-// Close DB connection:
-$mysqli->close();
+$mysqli->close(); // Close DB connection
 
-// Normal exit:
-exit(0);
+exit(0); // Normal exit
+
+// vim: noexpandtab shiftwidth=4 softtabstop=4 tabstop=4
