901347cdde71601c711c26c357a4c18e88b362a3
PixEye <pixeye@pixeye.net>
2023-04-15 20:19:58 +0200

    add few logs to the shell script

--- src/jsb.sh
+++ src/jsb.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
+pauseInSec=1
 src_dir=`dirname "$0"`
 cd "$src_dir/.." || exit $?
 
 while true # infinite loop to run it again after a fatal error
 do
+	echo "Update the code..."
 	git pull || exit $?
-	sleep 1
+
+	echo
+	echo "Wait $pauseInSec second..."
+	sleep $pauseInSec
+
+	echo
 	date > ~/last-start.txt
 	node src
+
+	echo "Exit code was: $?"
 done
 
 # vim: noexpandtab
