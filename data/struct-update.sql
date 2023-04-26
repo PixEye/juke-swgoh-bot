3c1c4d15ac063464c578ab7a34a40b8e04770f1f
PixEye <pix@pixeye.net>
2023-04-26 23:03:15 +0200

    fix self_guild_id for tw_results table

--- data/struct-update.sql
+++ data/struct-update.sql
@@ -9,7 +9,12 @@ ALTER TABLE `users` CHANGE `g12Count` `g12Count` INT(4) UNSIGNED NULL DEFAULT NU
 ALTER TABLE `users` CHANGE `g13Count` `g13Count` INT(4) UNSIGNED NULL DEFAULT NULL;
 ALTER TABLE `users` CHANGE `zetaCount` `zetaCount` INT(4) UNSIGNED NULL DEFAULT NULL;
 
-ALTER TABLE `users` CHANGE `guildRefId` `guildRefId` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
-ALTER TABLE `guilds` CHANGE `swgoh_id` `swgoh_id` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
+ALTER TABLE `users` CHANGE `guildRefId` `guildRefId`
+ VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `guilds` CHANGE `swgoh_id` `swgoh_id`
+ VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
 
 ALTER TABLE `guilds` ADD UNIQUE(`name`);
+
+ALTER TABLE `tw_results` CHANGE `self_guild_id` `self_guild_id`
+ VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
